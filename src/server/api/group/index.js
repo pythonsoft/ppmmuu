dcafea2a1b5586622cab8ba8d0f096a85e850f1c
chaoningx <chaoningx@phoenixtv.com>
2017-06-30 18:03:41 +0800

    添加组织管理接口文件

---
+++ src/server/api/group/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by steven on 17/5/5.
+ */
+const express = require('express');
+const router = express.Router();
+const result = require('../../common/result');
+const service = require('./service');
+
+module.exports = router;
