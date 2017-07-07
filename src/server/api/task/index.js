868d4a46cf95a6fceda7e1e800a43fe984745c76
chaoningx <chaoningx@phoenixtv.com>
2017-07-07 15:34:54 +0800

    添加任务API接口完成。

--- src/server/api/task/index.js
+++ src/server/api/task/index.js
@@ -11,7 +11,68 @@ const result = require('../../common/result');
 const service = require('./service');
 const isLogin = require('../../middleware/login');
 
+const i18n = require('i18next');
+
+const TaskInfo = require('./taskInfo');
+
 router.use(isLogin.middleware);
 router.use(isLogin.hasAccessMiddleware);
+
+router.get('/listAllParentTask', (req, res) => {
+  const page = req.query.page || 1;
+  const pageSize = req.query.pageSize || 30;
+  const sortFields = req.query.sortFields || '';
+  const fieldsNeed = req.query.fieldsNeed || '';
+  const status = req.query.status || '';
+  const userId = req.ex.userInfo._id;
+
+  service.listAllParentTask(userId, status, page, pageSize, (err, docs) => res.json(result.json(err, docs)), sortFields, fieldsNeed);
+});
+
+router.get('/listChildTask', (req, res) => {
+  const ids = req.query.ids;
+  const sortFields = req.query.sortFields || '';
+  const fieldsNeed = req.query.fieldsNeed || '';
+
+  service.listChildTask(ids, (err, docs) => res.json(result.json(err, docs)), sortFields, fieldsNeed);
+});
+
+router.get('/getTaskDetail', (req, res) => {
+  service.getTaskDetail(req.query.id, (err, doc) => res.json(result.json(err, doc)));
+});
+
+router.post('/addParentTask', (req, res) => {
+  const info = req.body;
+
+  if (!(info.creator && info.creator.type === [TaskInfo.CREATOR_TYPE.TEAM, TaskInfo.CREATOR_TYPE.SYSTEM].indexOf(info.creator.type) !== -1)) {
+    info.creator = {
+      _id: req.ex.userInfo._id,
+      name: req.ex.userInfo.name,
+      type: TaskInfo.CREATOR_TYPE.USER
+    }
+  }
+
+  service.addTask(info, null, (err, r) => res.json(result.json(err, 'ok')));
+});
+
+router.post('/addChildTask', (req, res) => {
+  const parentId = req.body.parentId;
+  const childTasks = req.body.childTasks;
+
+  try {
+    const tasks = JSON.parse(childTasks);
+    service.addChildTasks(parentId, tasks, (err, r) => res.json(result.json(err, 'ok')));
+  } catch (e) {
+    return res.json(result.fail(i18n.t('jsonParseError')))
+  }
+});
+
+router.post('/updateTask', (req, res) => {
+  service.updateTask(req.body.id, req.body, (err, r) => res.json(result.json(err, 'ok')));
+});
+
+router.post('/deleteTask', (req, res) => {
+  service.deleteTasks(req.body.ids, (err, r) => res.json(result.json(err, 'ok')));
+});
 
 module.exports = router;
