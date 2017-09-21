832100916926d757e6d1da938229d867b6126e6b
chaoningx <chaoningx@phoenixtv.com>
2017-09-21 12:00:53 +0800

    fix

--- src/server/api/library/catalogInfo.js
+++ src/server/api/library/catalogInfo.js
@@ -14,12 +14,15 @@ class CatalogInfo extends DB {
 
     this.struct = {
       _id: { type: 'string', default() { return uuid.v1(); }, allowUpdate: false },
-      objectid: { type: 'string', validation: 'require' },
+      fileId: { type: 'string', validation: 'require' },
+      objectId: { type: 'string', validation: 'require' },
       englishName: { type: 'string', validation: 'require' },
       chineseName: { type: 'string', validation: 'require' },
       parent: { type: 'string', validation: 'require' },
       keyword: { type: 'string', validation: 'require' },
       source: { type: 'string', validation: 'require' },
+      inpoint: { type: 'number', validation: 'require' },
+      outpoint: { type: 'number', validation: 'require' },
       root: { type: 'string' },
       available: { type: 'string', default: () => CatalogInfo.AVAILABLE.NO },
       owner: { type: 'object', default() { return { _id: '', name: '' }; }  },
--- src/server/api/library/fileInfo.js
+++ src/server/api/library/fileInfo.js
@@ -15,19 +15,42 @@ class FileInfo extends DB {
     this.struct = {
       _id: { type: 'string', default() { return uuid.v1(); }, allowUpdate: false },
       name: { type: 'string', validation: 'require' },
-
-      creatorId: { type: 'string', validation: 'require', allowUpdate: false },
+      size: { type: 'number', validation: 'require' },
+      realPath: { type: 'number', validation: 'require' },
+      path: { type: 'number', validation: 'require' },
+      type: { type: 'string', allowUpdate: false, validation: 'require', default: () => FileInfo.TYPE.ORIGINAL },
       createdTime: { type: 'date', validation: 'require', allowUpdate: false },
-      type: { type: 'string', allowUpdate: false, validation: 'require', default: () => FileInfo.TYPE.DOWNLOAD },
-      modifyTime: { type: 'date', validation: 'require' },
+      available: { type: 'string', default: () => FileInfo.AVAILABLE.NO },
+      lastModifyTime: { type: 'date', validation: 'require' },
       description: { type: 'string' },
       details: { type: 'object' },
+
+      archivePath: { type: 'string', validation: 'require' },
+      status: { type: 'string', validation: 'require' },
     };
   }
 }
 
 FileInfo.TYPE = {
-  DOWNLOAD: '1',
+  ORIGINAL: '0',
+  LOW_BIT_VIDEO: '1',
+  SUBTITLE: '2',
+  THUMB: '3', //缩略图
+  OTHER: '4',
+};
+
+FileInfo.AVAILABLE = {
+  NO: '0',
+  YES: '1',
+};
+
+FileInfo.STATUS = {
+  UNKNOW: '0',
+  ONLINE: '1',
+  ONLINE_NEAR: '2',
+  NEAR: '3',
+  ONLINE_OFFLINE: '4',
+  OFFLINE: '5',
 };
 
 module.exports = FileInfo;
