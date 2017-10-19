5e71d09bd87d9a71b6d9662e7145972526da6853
Michael Donux <michaeldx2008@gmail.com>
2017-10-19 15:59:40 +0800

    add engine test

--- src/server/api/engine/test.js
+++ src/server/api/engine/test.js
@@ -9,8 +9,10 @@ chai.use(chaiHttp);
 const expect = chai.expect;
 const agent = chai.request.agent(app);
 
-setTimeout(function () {
+setTimeout(() => {
   describe('/engine', () => {
+    let groupId = '';
+
     before((done) => {
       agent
       .post('/user/login')
@@ -21,17 +23,78 @@ setTimeout(function () {
       });
     });
 
+    describe('POST /addGroup', () => {
+      it('should add a engine group', (done) => {
+        agent
+        .post('/engine/addGroup')
+        .send({ name: 'testGroup' })
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.body.status).to.equal('0');
+          done();
+        });
+      });
+    });
+
     describe('GET /listGroup', () => {
       it('should get all engine groups', (done) => {
         agent
         .get('/engine/listGroup')
         .end((err, res) => {
           expect(res).to.have.status(200);
+          expect(res.body.status).to.equal('0');
+          groupId = res.body.data.docs[0]._id;
           done();
         });
       });
     });
+
+    describe('GET /getGroup', () => {
+      it('should get a engine group by id', (done) => {
+        agent
+        .get('/engine/getGroup')
+        .query({ groupId })
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.body.status).to.equal('0');
+          done();
+        });
+      });
+    });
+
+    describe('POST /updateGroup', () => {
+      it('should update a engine group by id', (done) => {
+        agent
+        .post('/engine/updateGroup')
+        .send({ groupId, name: 'updatedTestGroup' })
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.body.status).to.equal('0');
+          done();
+        });
+      });
+    });
+
+    describe('POST /removeGroup', () => {
+      it('should remove a engine group by id', (done) => {
+        agent
+        .post('/engine/removeGroup')
+        .send({ groupId })
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.body.status).to.equal('0');
+          done();
+        });
+      });
+    });
+
+    describe('GET /listEngine', () => {
+      it('should list engines', (done) => {
+        agent
+        .get('')
+      })
+    });
   });
 
   run();
-}, 10000);
+}, 2000);
