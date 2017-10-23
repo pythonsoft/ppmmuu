1a1de157ce9ec109bee72ea643ca1c873956f2f7
steven <1035968349@qq.com>
2017-10-23 18:40:56 +0800

    fix 订阅信息展示

--- src/server/api/subscribe/fieldConfig.js
+++ src/server/api/subscribe/fieldConfig.js
@@ -1,17 +1,14 @@
 'use strict';
 
 module.exports = {
+  FIELD330: {
+    cn: 'Slug',
+  },
+  programNO: {
+    cn: '節目編號',
+  },
   name: {
     cn: '節目名稱',
-  },
-  subscribeType: {
-    cn: '節目類型',
-  },
-  source: {
-    cn: '來源',
-  },
-  limit: {
-    cn: '限制',
   },
   FIELD195: {
     cn: '節目名稱(中文)',
@@ -19,26 +16,26 @@ module.exports = {
   FIELD196: {
     cn: '節目名稱(英文)',
   },
-  FIELD323: {
-    cn: '版本',
-  },
   FIELD197: {
     cn: '節目副題(中文)',
   },
   FIELD198: {
     cn: '節目副題(英文)',
   },
-  FIELD330: {
-    cn: 'Slug',
+  source: {
+    cn: '來源',
   },
-  programNO: {
-    cn: '節目編號',
+  limit: {
+    cn: '限制',
+  },
+  subscribeType: {
+    cn: '訂閱類型',
+  },
+  FIELD323: {
+    cn: '版本',
   },
   lastModifyTime: {
     cn: '上架时间',
-  },
-  FIELD162: {
-    cn: '新聞日期',
   },
   FIELD36: {
     cn: '首播日期',
@@ -46,11 +43,17 @@ module.exports = {
   FIELD183: {
     cn: '播出日/集數',
   },
+  FIELD162: {
+    cn: '新聞日期',
+  },
   FIELD03: {
     cn: '內容介紹',
   },
   FIELD321: {
     cn: '故事大纲',
+  },
+  FIELD247: {
+    cn: 'REMARK(内容介绍)',
   },
 };
 
