e89b6d4d116fdaef67cc099d53011f3aee3b428d
chaoningx <chaoningx@phoenixtv.com>
2017-10-27 17:25:33 +0800

    Merge branch 'master' of http://gitlab.szdev.cn/dev/UMP
    # Conflicts:
    #	src/server/api/help/service.js
    #	src/server/api/help/storage.js

--- src/server/api/help/service.js
+++ src/server/api/help/service.js
@@ -17,24 +17,23 @@ const fs = require('fs');
 const exec = require('child_process').exec;
 
 const VersionInfo = require('./versionInfo');
+
 const versionInfo = new VersionInfo();
 
 const service = {};
 
 const unzipPackage = function unzipPackage(id, zipPath, targetDirPath, cb) {
-  if(!fs.existsSync(targetDirPath)) {
+  if (!fs.existsSync(targetDirPath)) {
     fs.mkdirSync(targetDirPath);
   }
 
   const readStream = fs.createReadStream(zipPath);
   const unzipExtractor = unzip.Extract({ path: targetDirPath });
 
-  unzipExtractor.on('error', function(err) {
+  unzipExtractor.on('error', (err) => {
     logger.error(err.message);
 
-    service.update(id, VersionInfo.STATUS.ERROR, err.message, false, () => {
-      return cb && cb(err.message);
-    });
+    service.update(id, VersionInfo.STATUS.ERROR, err.message, false, () => cb && cb(err.message));
   });
 
   unzipExtractor.on('close', () => {
@@ -56,9 +55,9 @@ const copyFile = function copyFile(originPath, targetPath, cb) {
 };
 
 const readConfigJSON = function readConfigJSON(id, configPath, cb) {
-  if(fs.existsSync(configPath)) {
+  if (fs.existsSync(configPath)) {
     fs.readFile(configPath, { encoding: 'utf8' }, (err, data) => {
-      if(err) {
+      if (err) {
         logger.error(err.message);
         return cb && cb(err.message);
       }
@@ -68,10 +67,10 @@ const readConfigJSON = function readConfigJSON(id, configPath, cb) {
       versionInfo.collection.updateOne({ _id: id }, {
         $set: {
           version: obj.version,
-          updateList: obj.updateList
-        }
+          updateList: obj.updateList,
+        },
       }, (err, r) => {
-        if(err) {
+        if (err) {
           logger.error(err.message);
           return cb && cb(err.message);
         }
@@ -85,12 +84,12 @@ const readConfigJSON = function readConfigJSON(id, configPath, cb) {
 };
 
 const restartServer = function restartServer(id, cb) {
-  exec(`pm2 reload all`, (error, stdout, stderr) => {
+  exec('pm2 reload all', (error, stdout, stderr) => {
     if (error) {
       return cb && cb(error);
     }
 
-    if(stderr) {
+    if (stderr) {
       return cb && cb(stderr);
     }
 
@@ -99,7 +98,7 @@ const restartServer = function restartServer(id, cb) {
 };
 
 service.upload = function (info, creatorId, creatorName, cb) {
-  if(!info._id) {
+  if (!info._id) {
     info._id = uuid.v1();
   }
   const t = new Date();
@@ -112,46 +111,44 @@ service.upload = function (info, creatorId, creatorName, cb) {
   versionInfo.insertOne(info, (err, rs) => {
     if (err) {
       logger.error(err.message);
-      return cb && cb(i18n.t('databaseErrorDetail', { error: err.message}));
+      return cb && cb(i18n.t('databaseErrorDetail', { error: err.message }));
     }
 
     const zipPath = info.packagePath;
     const targetDirPath = path.join(config.uploadPackageTempPath, info._id);
 
-    unzipPackage(info._id, zipPath, targetDirPath, (err, r) => {
-      return cb && cb(err, r);
-    });
+    unzipPackage(info._id, zipPath, targetDirPath, (err, r) => cb && cb(err, r));
 
     return false;
   });
 };
 
 service.install = function install(id, cb) {
-  if(!id) {
-    //todo
+  if (!id) {
+    // todo
     return cb && cb(i18n.t('databaseErrorDetail', { error: 'id is null.' }));
   }
 
   versionInfo.collection.findOne({ _id: id }, { fields: { extractPath: 1 } }, (err, doc) => {
     if (err) {
       logger.error(err.message);
-      return cb && cb(i18n.t('databaseErrorDetail', { error: err.message}));
+      return cb && cb(i18n.t('databaseErrorDetail', { error: err.message }));
     }
 
-    if(!doc) {
+    if (!doc) {
       return cb && cb(i18n.t('databaseErrorDetail', { error: 'doc is null.' }));
     }
 
     fs.readdir(doc.extractPath, (err, files) => {
-      if(err) {
-        //todo
+      if (err) {
+        // todo
         return cb && cb(i18n.t('databaseErrorDetail', { error: err.message }));
       }
 
-      const loop = function(index) {
-        if(files[index]) {
-          service.update(id, VersionInfo.STATUS.TRANSFER, 'transfer completely', false, err => {
-            if(err) {
+      const loop = function (index) {
+        if (files[index]) {
+          service.update(id, VersionInfo.STATUS.TRANSFER, 'transfer completely', false, (err) => {
+            if (err) {
               return cb && cb(err);
             }
 
@@ -162,19 +159,17 @@ service.install = function install(id, cb) {
 
         const targetPath = globalConfig.upgradeSystem[files[index]];
 
-        if(targetPath) {
-          copyFile(path.join(doc.extractPath, files[index]) + '/', targetPath, err => {
-            if(err) {
-              service.update(id, VersionInfo.STATUS.ERROR, err, false, () => {
-                return cb && cb(err);
-              });
+        if (targetPath) {
+          copyFile(`${path.join(doc.extractPath, files[index])}/`, targetPath, (err) => {
+            if (err) {
+              service.update(id, VersionInfo.STATUS.ERROR, err, false, () => cb && cb(err));
             }
 
             loop(index + 1);
           });
-        }else if(files[index] === 'config.json') {
-          readConfigJSON(id, path.join(doc.extractPath, files[index]), err => {
-            if(err) {
+        } else if (files[index] === 'config.json') {
+          readConfigJSON(id, path.join(doc.extractPath, files[index]), (err) => {
+            if (err) {
               return cb && cb(err);
             }
             loop(index + 1);
@@ -189,26 +184,26 @@ service.install = function install(id, cb) {
 
 service.update = function (id, status, description, extractPath, cb) {
   const updateInfo = {};
-  if(status) {
+  if (status) {
     updateInfo.status = status;
   }
 
-  if(description) {
+  if (description) {
     updateInfo.description = description;
   }
 
-  if(extractPath) {
+  if (extractPath) {
     updateInfo.extractPath = extractPath;
   }
 
-  if(utils.isEmptyObject(updateInfo)) {
+  if (utils.isEmptyObject(updateInfo)) {
     return cb && cb(null, 'ok');
   }
 
   versionInfo.updateOne({ _id: id }, updateInfo, (err, rs) => {
     if (err) {
       logger.error(err.message);
-      return cb && cb(i18n.t('databaseErrorDetail', { error: err.message}));
+      return cb && cb(i18n.t('databaseErrorDetail', { error: err.message }));
     }
 
     return cb && cb(null, rs);
@@ -216,34 +211,33 @@ service.update = function (id, status, description, extractPath, cb) {
 };
 
 service.list = function (id, pathName, cb) {
-  if(!id) {
+  if (!id) {
     return cb && cb(i18n.t('databaseErrorDetail', { error: 'id is null.' }));
   }
 
   versionInfo.collection.findOne({ _id: id }, { fields: { extractPath: 1 } }, (err, doc) => {
     if (err) {
       logger.error(err.message);
-      return cb && cb(i18n.t('databaseErrorDetail', { error: err.message}));
+      return cb && cb(i18n.t('databaseErrorDetail', { error: err.message }));
     }
 
-    if(!doc) {
+    if (!doc) {
       return cb && cb(i18n.t('databaseErrorDetail', { error: 'doc is null.' }));
     }
 
     fs.readdir(path.join(doc.extractPath, pathName), (err, files) => {
-      if(err) {
+      if (err) {
         return cb && cb(i18n.t('databaseErrorDetail', { error: err.message }));
       }
 
       return cb && cb(null, files);
     });
-
   });
 };
 
 service.readFile = function readFile(id, filePath, res) {
-  if(!id) {
-    return res.end('id is null.' );
+  if (!id) {
+    return res.end('id is null.');
   }
 
   versionInfo.collection.findOne({ _id: id }, { fields: { extractPath: 1 } }, (err, doc) => {
@@ -252,8 +246,8 @@ service.readFile = function readFile(id, filePath, res) {
       return res.end('err.message');
     }
 
-    if(!doc) {
-      return res.end('version info is null.' );
+    if (!doc) {
+      return res.end('version info is null.');
     }
 
     fs.createReadStream(path.join(doc.extractPath, filePath)).pipe(res);
--- src/server/api/help/storage.js
+++ src/server/api/help/storage.js
@@ -4,23 +4,23 @@ const multer = require('multer');
 const config = require('./config');
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
+  destination(req, file, cb) {
     cb(null, config.uploadPackagePath);
   },
-  filename: function (req, file, cb) {
-    cb(null, new Date().getTime() + '_' +file.originalname);
+  filename(req, file, cb) {
+    cb(null, `${new Date().getTime()}_${file.originalname}`);
   },
 });
 
 const upload = multer({
-  storage: storage,
-  fileFilter: function (req, file, cb) {
-    if(file.mimetype !== 'application/zip') {
+  storage,
+  fileFilter(req, file, cb) {
+    if (file.mimetype !== 'application/zip') {
       cb(null, false);
-    }else {
-      cb (null, true);
+    } else {
+      cb(null, true);
     }
-  }
+  },
 });
 
 module.exports = { upload };
