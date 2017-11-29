d7cd8a7b1054f077f853be2354c098c45cc3b93f
谢朝宁 <chaoningx@163.com>
2017-11-29 10:22:18 +0800

    Merge branch 'master' of http://gitlab.szdev.cn/dev/UMP
    # Conflicts:
    #	src/server/api/help/index.js
    #	src/server/api/help/service.js
    #	src/server/api/help/storage.js
    #	src/server/api/help/versionInfo.js
    #	src/server/config_master.js

--- src/server/mongodbScript/syncIMAccount.js
+++ src/server/mongodbScript/syncIMAccount.js
@@ -4,7 +4,7 @@ const UserInfo = require('../api/user/userInfo');
 const userInfo = new UserInfo();
 
 const request = require('request');
-const TICKET = '8649e68b4adc426b6d8eda111be86f446b66d0f6b89f40334ef5fb6ad1b567c57971ca985a8b7bb9ff39b546de4a959984b97880257b83c99b9cbe407ba8797d';
+const TICKET = "8649e68b4adc426b6d8eda111be86f446b66d0f6b89f40334ef5fb6ad1b567c51b9897f916f5e54eaf3d1c97c570923a7acac0ad056baa01bbc5ae0115b6879d";
 
 userInfo.collection.find({}).toArray((err, docs) => {
   if(err) {
@@ -15,7 +15,7 @@ userInfo.collection.find({}).toArray((err, docs) => {
   for(let i = 0, len = docs.length; i < len; i++) {
 
     request.post({ url: 'http://localhost:9000/im/sync', form: {
-      ticket: TICKET,
+      'im-ticket': TICKET,
       key: 'ump',
       id: docs[i]._id,
       name: docs[i].name,
